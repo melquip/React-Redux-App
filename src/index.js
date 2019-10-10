f534f04d3c22fbb1d435ab1e82eab2ed841e67e1
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-10 14:27:52 +0100

    Fixed redux store and added middleware thunk

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import './index.css';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
 import * as reducers from './state/reducers';
-import throttle from 'lodash/throttle';
 import * as storage from './helpers/useLocalStorage';
+import thunk from 'redux-thunk';
+import throttle from 'lodash/throttle';
 
 const store = createStore(
 	combineReducers({
-		doggos: reducers.doggosReducer
+		doggos: reducers.doggosReducer,
 	}),
 	storage.loadState('doggos'),
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	compose(
+		applyMiddleware(thunk),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
 )
 
 store.subscribe(throttle(() => {
