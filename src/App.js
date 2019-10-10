e3d37396ab3010df53af1d1d84be399374367a50
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-10 13:02:43 +0100

    Displayed data in App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useSelector } from 'react-redux';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { doggos } = useSelector(state => state);
+	return (
+		<div className="doggos">
+			{
+				doggos.length ? doggos.map(doggo => (
+					<div className="dog">
+						<img src={doggo} alt="" />
+					</div>
+				)) : null
+			}
+		</div>
+	);
 }
 
 export default App;
